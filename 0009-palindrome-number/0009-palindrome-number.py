61d50f2f23e55e13c4ba9f1347f58d9dabc3be2c
leefox23 <130342901+leefox23@users.noreply.github.com>
2024-04-09 18:51:52 +0900

    Delete 0009-palindrome-number directory

--- 0009-palindrome-number/0009-palindrome-number.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-    # 음수는 회문이 될 수 없으므로 바로 False를 반환
-        if x < 0:
-            return False
-    # x를 문자열로 변환
-        str_x = str(x)
-    # 문자열의 앞뒤를 비교하여 회문인지 확인
-        return str_x == str_x[::-1]

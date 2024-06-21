4856c1b7367265838b4587f1efcd9cb3e3d8ffa1
leefox23 <130342901+leefox23@users.noreply.github.com>
2024-06-21 12:37:29 +0900

    Time: 43 ms (35.73%), Space: 16.4 MB (87.34%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.py
+++ 0507-perfect-number/0507-perfect-number.py
@@ -1,3 +1,9 @@
 class Solution:
     def checkPerfectNumber(self, num: int) -> bool:
-        return num in [6,28,496,8128,33550336]
+        if num==1:
+            return False
+        count=1
+        for i in range(2,int(num**0.5)+1):
+            if num%i==0:
+                count+=i+num//i
+        return num==count

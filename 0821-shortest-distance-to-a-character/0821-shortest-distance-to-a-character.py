de19d643238046fd43f6b64c7652a75157f522c5
leefox23 <130342901+leefox23@users.noreply.github.com>
2024-06-21 13:14:56 +0900

    Time: 35 ms (92.71%), Space: 16.5 MB (78.60%) - LeetHub

---
+++ 0821-shortest-distance-to-a-character/0821-shortest-distance-to-a-character.py
@@ -0,0 +1,18 @@
+class Solution:
+    def shortestToChar(self, S: str, C: str) -> List[int]:
+        shortest_dist = []
+        size = len(S)
+        if size == 1:
+            return [0]
+        for idx, char in enumerate(S):
+            if char == C:
+                shortest_dist.append(0)
+            else:
+                if idx == 0:
+                    shortest_dist.append( size )
+                else:
+                    shortest_dist.append( shortest_dist[-1] + 1)           
+        for idx in range(2, size+1):
+
+            shortest_dist[-idx] = min(shortest_dist[-idx], shortest_dist[-idx+1]+1 )
+        return shortest_dist

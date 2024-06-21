8e0c558af35e78fde8458fa59c5fdcee5dfd2291
leefox23 <130342901+leefox23@users.noreply.github.com>
2024-06-21 13:28:10 +0900

    Time: 32 ms (79.73%), Space: 16.3 MB (99.78%) - LeetHub

---
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def dfsInorder(self, root):
+        if root is None: return
+        self.dfsInorder(root.left)
+        self.represented.append(root.val)
+        self.dfsInorder(root.right)
+
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        self.represented = []
+        self.dfsInorder(root)
+
+        pointer = answer = TreeNode(self.represented[0])
+        for node in self.represented[1:]:
+            pointer.right = TreeNode(node)
+            pointer = pointer.right
+
+        return answer

55e1671c8578f48b40bbe267f3f816ae00a73658
leefox23 <130342901+leefox23@users.noreply.github.com>
2024-06-21 12:30:21 +0900

    Time: 39 ms (85.44%), Space: 17.2 MB (45.81%) - LeetHub

---
+++ 0482-license-key-formatting/0482-license-key-formatting.py
@@ -0,0 +1,10 @@
+class Solution:
+    def licenseKeyFormatting(self, S: str, K: int) -> str:
+        S = S.replace('-', '')
+        head = len(S) % K
+        grouping = []
+        if head:
+            grouping.append( S[:head] )
+        for index in range(head, len(S), K ):
+            grouping.append( S[ index : index+K ] )        
+        return '-'.join( grouping ).upper()

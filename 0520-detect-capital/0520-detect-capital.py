2aa0c999830bd987a84c0a3bc2093a716288e896
leefox23 <130342901+leefox23@users.noreply.github.com>
2024-06-21 12:38:46 +0900

    Time: 44 ms (6.60%), Space: 16.3 MB (99.32%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.py
@@ -0,0 +1,8 @@
+class Solution:
+    def detectCapitalUse(self, word: str) -> bool:
+	    cnt = sum(c.isupper() for c in word)
+
+	    return cnt == len(word) \
+		    or cnt == 0 \
+		    or cnt == 1 and word[0].isupper()
+        
